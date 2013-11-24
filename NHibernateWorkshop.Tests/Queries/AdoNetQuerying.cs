6ed0380b283ad0e24ce5a9faf5cfea243751b1dd
Stein Jakob Nordbø <steinjak@gmail.com>
2013-11-24 22:08:05 +0100

    Added an ado.net example

---
+++ NHibernateWorkshop.Tests/Queries/AdoNetQuerying.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using NHibernateWorkshop.Models;
+using NUnit.Framework;
+
+namespace NHibernateWorkshop.Tests.Queries
+{
+    [TestFixture]
+    public class AdoNetQuerying
+    {
+        [Test, Explicit]
+        public void DoItTheAdoNetWay()
+        {
+            var localhost = "Data Source=(localdb)\\Projects;Initial Catalog=NHibernateWorkshop;Integrated Security=True";
+            var sql = "select b.Id, b.Title, b.OwnerId, u.Username, u.Name " +
+                        "from [Blog] b inner join [User] u on u.Id = b.OwnerId " +
+                        "where b.Title like @title";
+
+            var blogs = new List<Blog>();
+            using (var connection = new SqlConnection(localhost))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    using (var query = new SqlCommand(sql, connection, transaction))
+                    {
+                        query.Parameters.Add("@title", SqlDbType.NVarChar, 255).Value = "%and%";
+                        using (var results = query.ExecuteReader())
+                        {
+                            while (results.Read())
+                            {
+                                blogs.Add(new Blog
+                                {
+                                    Id = results.GetGuid(0),
+                                    Title = results.GetString(1),
+                                    Owner = new User
+                                    {
+                                        Id = results.GetGuid(2),
+                                        Username = results.GetString(3),
+                                        Name = results.GetString(4)
+                                    }
+                                });
+                            }
+                        }
+                        transaction.Commit();
+                    }
+                }
+            }
+
+            foreach (var blog in blogs)
+            {
+                Console.WriteLine("Blog: `{0}' by {1}", blog.Title.Replace('\n', ' '), blog.Owner.Name);
+            }
+        }
+    }
+}
